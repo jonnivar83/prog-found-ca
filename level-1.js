d7750f624ecc4af63d4abc493e8ba651b0386bab
jonnivar83 <58147335+jonnivar83@users.noreply.github.com>
2020-03-15 22:49:16 +0100

    Add files via upload

---
+++ level-1.js
@@ -0,0 +1,66 @@
+// question 1
+var cat ="NonStop";
+// question 2
+var person = {
+    name: "thePope",
+    age: 37
+};
+// question 3
+var outofstock = true;
+if(outofstock===true){
+console.log("Out of Stock");
+}
+else {
+    console.log("In Stock");
+}
+// question 4
+var numbers = [1,2,3,4,5];
+console.log(1);
+console.log(2);
+console.log(3);
+console.log(4);
+console.log(5);
+// question 5
+for(var i = 15; i <= 25; i++) {
+    console.log(i);
+}
+// question 6
+for(var i = 15; i <= 25; i++) {
+    console.log(20);
+}
+// question 7
+var music = [
+{
+name:"AgorafobicNosebleed",
+age: 3,
+anygood: false},
+{
+name: "SRF", 
+age: 25, 
+anygood: true}];
+for(var i = 0; i < posts.length; i++) 
+{
+    console.log(posts[i].age);
+    console.log(posts[i].anygood);
+}
+// question 8
+function WhatIDontLike ()
+{
+    var taste =["i don't like"]
+taste.push=("snakes")
+console.log(taste);
+}
+WhatIDontLike();
+// question 9
+function subtracting (number1, number2)
+{var sum = number1 - number2;
+    console.log(sum);
+}
+    subtracting(5,3);
+// question 10
+function moreempty()
+{var sum = ["even"];
+    sum.push=("steven");
+    console.log(sum);
+}
+moreempty();
